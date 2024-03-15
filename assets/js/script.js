c77d6bc1f297d8f1ef687f163553d47ca524c1e3
Amin <124307503+Aminzaree@users.noreply.github.com>
2024-03-15 13:21:47 +0330

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,30 @@
 // meaning of colorScheme : The color scheme currently used in the Telegram app. Either “light” or “dark”.
 
 // This function is used to set the theme class on the HTML element of the web page.
-function setThemeClass(){ 
-    document.documentElement.className = Telegram.WebApp.colorScheme
-}
+// function setThemeClass(){ 
+//     document.documentElement.className = Telegram.WebApp.colorScheme
+// }
 
 // Controller (onEvent) for themeChnaged.
 // When the theme changes, setThemeClass() is called.
-Telegram.WebApp.onEvent("themeChanged", setThemeClass);
-setThemeClass();
+// Telegram.WebApp.onEvent("themeChanged", setThemeClass);
+// setThemeClass();
+
+
+function changePaymentMethod(event){
+    let paymentMethodItem = document.getElementsByClassName("paymentMethod");
+    let i;
+    for(i = 0; i < paymentMethodItem.length; i++){
+        paymentMethodItem[i].classList.remove("active")
+    }
+    event.classList.add("active");
+}
+
+function changeAccountType(event){
+    let accountTypeItem = document.getElementsByClassName("accountType");
+    let i = 0;
+    for(i = 0; i < accountTypeItem.length; i++){
+        accountTypeItem[i].classList.remove("active");
+    }
+    event.classList.add("active");
+}
